266c6b66724a3ccd60cfa7ba5f93989f3273916d
binnsyGA <alice.binns17@gmail.com>
2019-07-19 16:14:41 -0400

    Updates

---
+++ precache-manifest.c290dd4e3428d494e8d156366feae634.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49b73a033e9fed0ab3db803b0e6a0cc",
+    "url": "/freight-farms-app/index.html"
+  },
+  {
+    "revision": "6975d05dbb6804c10305",
+    "url": "/freight-farms-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7fdc8a6050faee7137b2",
+    "url": "/freight-farms-app/static/css/main.c6aa70a9.chunk.css"
+  },
+  {
+    "revision": "6975d05dbb6804c10305",
+    "url": "/freight-farms-app/static/js/2.571fd8a8.chunk.js"
+  },
+  {
+    "revision": "7fdc8a6050faee7137b2",
+    "url": "/freight-farms-app/static/js/main.ddc21dc6.chunk.js"
+  },
+  {
+    "revision": "9585283e555022ce6905",
+    "url": "/freight-farms-app/static/js/runtime~main.50cb84ff.js"
+  },
+  {
+    "revision": "e8b334bc766ccc1fd414c7a8c900e0e7",
+    "url": "/freight-farms-app/static/media/earth.e8b334bc.jpg"
+  },
+  {
+    "revision": "f58747deb79fdf62d5e3a3794f558b18",
+    "url": "/freight-farms-app/static/media/lettuce.f58747de.jpeg"
+  }
+]);
